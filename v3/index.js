7a67cd19e481c8a612497ca0e484c1f605416483
Basil21777 <basel.yaser205@gmail.com>
2025-12-04 22:08:24 +0200

    version 3

---
+++ v3/index.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require('express');
+const morgan = require('morgan');
+const coursesRouter = require('./routes/courses.routes.js'); // <-- no { router }
+const {ERROR}=require('./utils/httpStatusText')
+const app = express();
+const cors = require('cors');
+const mongoose=require('mongoose');
+
+// console.log(process);
+
+const url = process.env.MONGO_URL ;
+
+mongoose.connect(url).then(()=>{
+  console.log("Mongo BD Started ... ");
+});
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use('/api/courses/', coursesRouter);
+// app.use('/api/users', usersRouter);
+
+// global middleware handel not found routs wildcard
+app.all(/.*/,(req,res,next)=>{
+  res.status(404).json({status:ERROR,msg:"this resource is not available"});
+});
+
+// global middleware handel errors
+app.use((err,req,res,next)=>{
+  res.status(err.statusCode||500).json({status:err.statusText||ERROR,msg:err.message,data:null})
+});
+
+
+
+
+const port=process.env.PORT;
+app.listen(port||5000, () => {
+  console.log("Listening on 5000...");
+});
